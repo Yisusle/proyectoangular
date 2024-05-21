b72b461bada1a523ae4980a202e8fbf5c5a28dc2
Jesus Leyva <jesusle116@gmail.com>
2024-05-20 18:43:11 -0700

    Rutas

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,3 +1,8 @@
 export var Global ={
-    url: 'backend.ex0oqns.mongodb.net/portafolio/'
+    
+    /*usernameMongo:  encodeURIComponent("admin"),
+    passwordMongo: encodeURIComponent("admin"),
+
+    url: `mongodb+srv://${encodeURIComponent("admin")}:${encodeURIComponent("admin")}@enlace/?retryWrites=true&w=majority&appName=backend`*/
+    url: "https://backend-ruddy-kappa-42.vercel.app"
 };
