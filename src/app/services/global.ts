819c4288d25685311927c3ef9651031792ab8911
Jesus Leyva <jesusle116@gmail.com>
2024-05-20 18:49:24 -0700

    Rutasv2

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -4,5 +4,5 @@ export var Global ={
     passwordMongo: encodeURIComponent("admin"),
 
     url: `mongodb+srv://${encodeURIComponent("admin")}:${encodeURIComponent("admin")}@enlace/?retryWrites=true&w=majority&appName=backend`*/
-    url: "https://backend-ruddy-kappa-42.vercel.app/api"
+    url: "https://backend-ruddy-kappa-42.vercel.app/api/"
 };
